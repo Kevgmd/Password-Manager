98cb125168fe7a26131e218ae47907206e700368
Kevin <140569159+Kevgmd@users.noreply.github.com>
2024-02-26 22:11:12 +0000

    daily commit 1

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 //Buttons focus style
-document.querySelectorAll("button").forEach(function(button) {
-    button.addEventListener("mousedown", function() {
+document.querySelectorAll("button").forEach(function (button) {
+    button.addEventListener("mousedown", function () {
         this.style.backgroundColor = "#3d3d3d";
     });
 
-    button.addEventListener("mouseup", function() {
+    button.addEventListener("mouseup", function () {
         this.style.backgroundColor = "";
     });
-    button.addEventListener("mouseleave", function() {
+    button.addEventListener("mouseleave", function () {
         this.style.backgroundColor = "";
     });
 });
 //onclick functions
 function newPassword() {
-    var element = document.getElementsByClassName("insert-container")[0];
+    let element = document.getElementsByClassName("insert-container")[0];
     element.style.display = "flex";
 }
 function abort() {
     let element = document.getElementsByClassName("insert-container")[0];
-    
+
     element.style.display = "none";
-    
-    document.querySelectorAll("input").forEach(function(input) {
+    document.getElementById("inputAlert").style.display = "none";
+
+    document.querySelectorAll("input").forEach(function (input) {
         input.value = "";
     });
 }
@@ -31,7 +32,7 @@ let insertedValues = document.querySelectorAll("input");
 function checkInputs() {
     let inputsFilled = true;
 
-    insertedValues.forEach(function(input) {
+    insertedValues.forEach(function (input) {
         if (!input.value.trim()) {
             inputsFilled = false;
         }
@@ -39,16 +40,21 @@ function checkInputs() {
 
     if (inputsFilled) {
         createDiv();
+    } else {
+        document.getElementById("inputAlert").style.display = "flex";
     }
 }
 
 function createDiv() {
     var newDiv = document.createElement("div");
+    var newH1 = document.createElement("h1");
+
     newDiv.className = "password";
+
     document.getElementById("passwords-container").appendChild(newDiv);
+    document.getElementsByClassName("password").appendChild(newH1);
 
     let element = document.getElementsByClassName("insert-container")[0];
-    console.log(insertedValues);
 
     element.style.display = "none";
 
